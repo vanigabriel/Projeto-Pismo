a1188d58fa3813af20c82b0b4092e5a64e3b3917
Gabriel Angelo Vani <vani.gabr@gmail.com>
2019-10-19 21:23:43 -0300

    add CreditAvaliable

--- entity/account.go
+++ entity/account.go
@@ -2,7 +2,8 @@ package entity
 
 // Account definition struct
 type Account struct {
-	AccountID      int
-	CreditLimit    float64 `json:"available_credit_limit" valid:"required"`
-	WithdrawlLimit float64 `json:"available_withdrawal_limit" valid:"required"`
+	AccountID       int
+	CreditLimit     float64 `json:"available_credit_limit" valid:"required"`
+	WithdrawlLimit  float64 `json:"available_withdrawal_limit" valid:"required"`
+	CreditAvaliable float64
 }
