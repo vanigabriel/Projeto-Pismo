617d1fd47c16159f058e8e30626b034163e319bd
Gabriel Angelo Vani <vani.gabr@gmail.com>
2019-10-19 23:56:54 -0300

    Update account.go

--- entity/account.go
+++ entity/account.go
@@ -5,5 +5,5 @@ type Account struct {
 	AccountID       int     `json:"account_id"`
 	CreditLimit     float64 `json:"available_credit_limit" valid:"optional"`
 	WithdrawlLimit  float64 `json:"available_withdrawal_limit" valid:"optional"`
-	CreditAvaliable float64 `json:"-"`
+	CreditAvaliable float64 `json:"available_balance"`
 }
