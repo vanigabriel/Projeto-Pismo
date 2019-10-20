43bcf40ca296d4d3de9cd75641d064b101a5256e
Gabriel Angelo Vani <vani.gabr@gmail.com>
2019-10-19 23:45:45 -0300

    Update api.go

--- api/api.go
+++ api/api.go
@@ -22,7 +22,7 @@ func SetupRouter(service *usecases.Service, log *timber.Client) *gin.Engine {
 		v1.POST("/accounts", CreateAccount(service, log))
 		v1.PATCH("/accounts/:id", AlterAccount(service, log))
 		v1.GET("/accounts/limits", RetriveAccounts(service, log))
-		v1.GET("/accounts/:id/transactions", RetriveTransactionsID(service, log))
+		v1.GET("/accounts/transactions/:id", RetriveTransactionsID(service, log))
 
 		v1.POST("/transactions", InsertTransaction(service, log))
 		v1.POST("/payments", InsertPayments(service, log))
