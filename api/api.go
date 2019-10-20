8d88f6f972e50a451a281aae1597f3083c831eff
Gabriel Angelo Vani <vani.gabr@gmail.com>
2019-10-19 22:44:43 -0300

    Create api.go

---
+++ api/api.go
@@ -0,0 +1,130 @@
+package api
+
+import (
+	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+	"github.com/vanigabriel/Projeto-Pismo/entity"
+	"github.com/vanigabriel/Projeto-Pismo/usecases"
+
+	"github.com/evalphobia/go-timber/timber"
+)
+
+// SetupRouter create router
+func SetupRouter(service *usecases.Service, log *timber.Client) *gin.Engine {
+	r := gin.Default()
+	gin.SetMode(gin.ReleaseMode)
+
+	v1 := r.Group("/v1")
+	{
+		v1.POST("/accounts", CreateAccount(service, log))
+		v1.PATCH("/accounts/:id", AlterAccount(service, log))
+		v1.GET("/accounts/limits", RetriveAccounts(service, log))
+	}
+
+	return r
+}
+
+// CreateAccount route to creating a new account
+func CreateAccount(service *usecases.Service, log *timber.Client) func(c *gin.Context) {
+	return func(c *gin.Context) {
+		var Income Account
+
+		err := c.BindJSON(&Income)
+		if err != nil {
+			log.Err(err.Error())
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		var Acc entity.Account
+
+		Acc.CreditLimit = Income.CreditLimit.Value
+		Acc.WithdrawlLimit = Income.WithdrawlLimit.Value
+
+		created, err := service.AddAccount(&Acc)
+		if err != nil {
+			log.Err(err.Error())
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		c.JSON(http.StatusCreated, created)
+
+	}
+}
+
+// AlterAccount route to patch account
+func AlterAccount(service *usecases.Service, log *timber.Client) func(c *gin.Context) {
+	return func(c *gin.Context) {
+		var id string
+		id = c.Param("id")
+		if len(id) == 0 {
+			log.Err("Account ID not informed")
+			c.JSON(http.StatusBadRequest, gin.H{"error": "Account ID not informed"})
+			return
+		}
+
+		idAccont, _ := strconv.Atoi(id)
+
+		var Income Account
+
+		err := c.BindJSON(&Income)
+		if err != nil {
+			log.Err(err.Error())
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		var Acc entity.Account
+
+		Acc.CreditLimit = Income.CreditLimit.Value
+		Acc.WithdrawlLimit = Income.WithdrawlLimit.Value
+
+		updated, err := service.UpdateAccount(idAccont, Acc.CreditLimit, Acc.WithdrawlLimit)
+		if err != nil {
+			log.Err(err.Error())
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+		c.JSON(http.StatusCreated, updated)
+	}
+}
+
+// RetriveAccounts route to get accounts limits
+func RetriveAccounts(service *usecases.Service, log *timber.Client) func(c *gin.Context) {
+	return func(c *gin.Context) {
+		Accounts, err := service.GetAccounts()
+		if err != nil {
+			log.Err(err.Error())
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		c.JSON(http.StatusOK, Accounts)
+	}
+}
+
+// InsertTransaction route to insert a new transaction
+func InsertTransactions(service *usecases.Service, log *timber.Client) func(c *gin.Context) {
+	return func(c *gin.Context) {
+		var Transaction entity.Transaction
+
+		err := c.BindJSON(&Transaction)
+		if err != nil {
+			log.Err(err.Error())
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		err = service.InsertTransaction(&Transaction)
+		if err != nil {
+			log.Err(err.Error())
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		c.JSON(http.StatusCreated, gin.H{})
+	}
+}
