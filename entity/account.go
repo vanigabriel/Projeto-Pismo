11ef6e92b88dc0ba037799342e02b2734095f954
Gabriel Angelo Vani <vani.gabr@gmail.com>
2019-10-19 23:40:37 -0300

    fix json tags

--- entity/account.go
+++ entity/account.go
@@ -2,8 +2,8 @@ package entity
 
 // Account definition struct
 type Account struct {
-	AccountID       int
+	AccountID       int     `json:"account_id"`
 	CreditLimit     float64 `json:"available_credit_limit" valid:"optional"`
 	WithdrawlLimit  float64 `json:"available_withdrawal_limit" valid:"optional"`
-	CreditAvaliable float64
+	CreditAvaliable float64 `json:"-"`
 }
--- entity/transaction.go
+++ entity/transaction.go
@@ -2,11 +2,11 @@ package entity
 
 // Transaction definition struct
 type Transaction struct {
-	TransactionID int
+	TransactionID int     `json:"transaction_id"`
 	AccountID     int     `json:"account_id" valid:"required"`
 	OperationID   int     `json:"operation_type_id" valid:"required"`
 	Amount        float64 `json:"amount" valid:"required"`
-	Balance       float64
-	EventDate     string
+	Balance       float64 `json:"balance"`
+	EventDate     string  `json:"event_date"`
 	DueDate       string
 }
